e920b57a430005435153dd85a0d3c82eb28449a6
clementlefevre <clement.san@gmail.com>
2016-12-29 14:35:50 +0100

    add shifted for all regularized predictors

--- test.py
+++ test.py
@@ -10,13 +10,17 @@ import csv
 import datetime
 
 
-cols =['Open_Ask_EURRUB', 'Open_Ask_USDRUB', 'Open_Ask_USDCAD', 'Open_Ask_LIGHTCMDUSD', 'Open_Ask_USDMXN', 'Open_Ask_EURNOK', 'Open_Ask_USDNOK', 'Open_Ask_BRENTCMDUSD']
+datasource_path = ''
+
+cols = ['Open_Ask_EURRUB', 'Open_Ask_USDRUB', 'Open_Ask_USDCAD', 'Open_Ask_LIGHTCMDUSD',
+        'Open_Ask_USDMXN', 'Open_Ask_EURNOK', 'Open_Ask_USDNOK', 'Open_Ask_BRENTCMDUSD']
+
 
 def compute(targets):
     for target in targets:
-        for shift in range(1,2):
-            simulator = Simulator(dt_from='2016-09-29 12:00', dt_to='2016-09-29 12:30', target=target, shift=shift,fit_model=True,
-                         datasource_path='/drive_ext4/merged_no_spread_light.csv',ticks_to_shift=[1,2,3,4,5,10,20,30])
+        for shift in [3, 50]:
+            simulator = Simulator(dt_from='2016-09-28 12:00', dt_to='2016-09-28 12:30', target=target, shift=shift, fit_model=True,
+                                  datasource_path=datasource_path, ticks_to_shift=[0, 1, 2, 3, 4, 5, 10, 20, 30, 40, 50])
             regressor.dataset(simulator)
             regressor.fit(simulator)
             regressor.predict(simulator)
@@ -25,7 +29,8 @@ def compute(targets):
 
 if __name__ == '__main__':
     try:
-        compute(['Open_Ask_EURRUB'])
+        compute(['Open_Ask_EURRUB', 'Open_Ask_USDRUB', 'Open_Ask_USDCAD', 'Open_Ask_LIGHTCMDUSD', 'Open_Ask_USDMXN',
+                 'Open_Ask_EURNOK', 'Open_Ask_USDNOK', 'Open_Ask_BRENTCMDUSD'])
     except Exception as e:
         logging.error('Error :')
         raise
